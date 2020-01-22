f35e7e6eb9a4de628556d773d0973b6522593780
David Behar <davbelom@gmail.com>
2020-01-22 13:43:37 -0600

    Category Component created

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import Category from './Components/Categories/Category.jsx'
+
+const App = () => {
+  return <Category></Category>
+}
+
+export default App
---
+++ src/Components/Categories/Category.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Anchor = styled.a`
+  display:flex;
+  flex-direction:column;
+  text-align:center;
+  text-decoration:none;
+  width: 75px;
+`;
+
+const Image =styled.img`
+  border:1px solid #ddd;
+  box-shadow:0px 10px 14px rgba(0,0,0,0.2);
+  border-radius:50%;
+  height: auto;
+  overflow:hidden;
+  object-fit:cover;
+  height: 75px;
+  width: 75px;
+`;
+
+const DEFAULT_IMAGE = 'https://i.imgur.com/dJa0Hpl.jpg'
+
+const Category = ({cover=DEFAULT_IMAGE, path, emoji='?'}) => {
+  return (
+    <Anchor href={path}>
+      <Image src={cover} />
+      {emoji}
+    </Anchor>
+  )
+}
+
+export default Category
